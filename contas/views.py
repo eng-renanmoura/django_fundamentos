f9ed461d78f02123b82db3057e268e296613b7d7
Renan Moura <eng.renanmoura@gmail.com>
2018-08-25 17:12:00 -0300

    fundamentos django

---
+++ contas/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .forms import TransacaoForm
+
+from django.http import HttpResponse
+import datetime
+from .models import Transacao
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1','t2','t3']
+    data['now'] = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+    #return HttpResponse(html)
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    form = TransacaoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    return render(request, 'contas/form.html', {'form':form})
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    return render(request, 'contas/form.html', {'form':form, 'transacao':transacao})
+
+def delete(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+
+    return redirect('url_listagem')
